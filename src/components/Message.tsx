f2149c5a7c1efe476f2b7eaeeca4c8ff1ce20087
João Victor da Silva <joaovictordasilva0911@gmail.com>
2023-07-21 14:49:32 -0300

    Merge

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -11,20 +11,24 @@ interface MessageProps {
 
 export function Message({ role, content }: MessageProps) {
   return (
-    <div className="flex py-4 last:pb-20 last:mb-40 justify-center max-w-3xl w-full gap-3 text-slate-600 text-sm border-b border-slate-300 last:border-transparent">
-      {role === 'user' && (
-        <Avatar>
-          <AvatarFallback>JV</AvatarFallback>
-          <AvatarImage src="https://github.com/joaovictor09.png" />
-        </Avatar>
-      )}
+    <div
+      data-role={role}
+      className="w-full flex justify-center py-4 last:pb-20 last:mb-40 data-[role=assistant]:bg-slate-200 data-[role=assistant]:border-b data-[role=assistant]:border-slate-400 "
+    >
+      <div className="flex justify-center max-w-3xl w-full gap-3 text-slate-600 text-sm">
+        {role === 'user' && (
+          <Avatar>
+            <AvatarFallback>JV</AvatarFallback>
+            <AvatarImage src="https://github.com/joaovictor09.png" />
+          </Avatar>
+        )}
 
-      {role === 'assistant' && (
-        <Avatar>
-          <AvatarFallback>RS</AvatarFallback>
-          <AvatarImage src="https://github.com/rocketseat.png" />
-        </Avatar>
-      )}
+        {role === 'assistant' && (
+          <Avatar>
+            <AvatarFallback>RS</AvatarFallback>
+            <AvatarImage src="https://github.com/rocketseat.png" />
+          </Avatar>
+        )}
 
       <p data-role={role} className="leading-relaxed w-full pb-5">
         <span className="block font-bold text-slate-700">
